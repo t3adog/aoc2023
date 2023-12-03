bcbdc1489135e255b4c29f9f11e26278a4296349
Petr Belyakov <keptelr@ya.ru>
2023-12-03 19:04:35 +0300

    day03 part01 completed

--- day03/Day03.go
+++ day03/Day03.go
@@ -1,7 +1,91 @@
 package day03
 
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/kptlr/aoc2023/utils"
+)
+
+var DigitsRegexp = regexp.MustCompile("\\.?[0-9]+\\.?")
+
+type Point struct {
+	y int
+	x int
+}
+
+type Square struct {
+	from Point
+	to   Point
+}
+
 func PartOne(input []string) (result int) {
-	return 0
+	for index, line := range input {
+		digits := DigitsRegexp.FindAllString(line, -1)
+		for _, digit := range digits {
+			isDetail := isDetail(input, digit, index)
+			input[index] = strings.Replace(input[index], digit, utils.GeneratePlug(len(digit), "."), 1) // Цифры могут повторяться. Так что глушим вхождения. Костыль, зато какой!
+			if isDetail {
+				d, _ := strconv.Atoi(strings.ReplaceAll(digit, ".", ""))
+				result += d
+			}
+		}
+	}
+	return
+}
+
+func isDetail(input []string, digit string, index int) bool {
+	searchSquare := parseSquare(input, digit, index)
+
+	for y := searchSquare.from.y; y <= searchSquare.to.y; y++ {
+		if len(strings.ReplaceAll(strings.ReplaceAll(input[y][searchSquare.from.x:searchSquare.to.x+1], digit, ""), ".", "")) > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func parseSquare(input []string, digit string, index int) (result Square) {
+	maxX := len(input[index]) - 1
+	maxY := len(input) - 1
+
+	digitIndex := strings.Index(input[index], digit)
+
+	// Ищем FROM
+	if strings.HasPrefix(digit, ".") {
+		result.from.x = digitIndex
+	} else {
+		if digitIndex-1 > 0 {
+			result.from.x = digitIndex - 1
+		} else {
+			result.from.x = 0
+		}
+	}
+
+	if index-1 >= 0 {
+		result.from.y = index - 1
+	} else {
+		result.from.y = 0
+	}
+
+	// Ищем TO
+	if strings.HasSuffix(digit, ".") {
+		result.to.x = digitIndex + (len(digit) - 1)
+	} else {
+		if digitIndex+(len(digit)-1)+1 <= maxX {
+			result.to.x = digitIndex + (len(digit) - 1) + 1
+		} else {
+			result.to.x = maxX
+		}
+	}
+
+	if index+1 <= maxY {
+		result.to.y = index + 1
+	} else {
+		result.to.y = maxY
+	}
+	return
 }
 
 func PartTwo(input []string) (result int) {
--- day03/Day03_test.go
+++ day03/Day03_test.go
@@ -10,11 +10,35 @@ import (
 func TestPartOne(t *testing.T) {
 	input := utils.ReadLines("test", "day03")
 	result := PartOne(input)
-	assert.Equal(t, 0, result)
+	assert.Equal(t, 4361, result)
 }
 
 func TestPartTwo(t *testing.T) {
 	input := utils.ReadLines("test", "day03")
 	result := PartTwo(input)
 	assert.Equal(t, 1, result)
+}
+
+func TestParseSquare(t *testing.T) {
+	input := utils.ReadLines("test", "day03")
+	sq := Square{}
+	sq.from.x = 0
+	sq.from.y = 0
+	sq.to.x = 3
+	sq.to.y = 1
+	assert.Equal(t, sq, parseSquare(input, "467.", 0))
+
+	sq1 := Square{}
+	sq1.from.x = 4
+	sq1.from.y = 8
+	sq1.to.x = 8
+	sq1.to.y = 9
+	assert.Equal(t, sq1, parseSquare(input, ".598.", 9))
+
+	sq3 := Square{}
+	sq3.from.x = 1
+	sq3.from.y = 1
+	sq3.to.x = 4
+	sq3.to.y = 3
+	assert.Equal(t, sq3, parseSquare(input, ".35.", 2))
 }
