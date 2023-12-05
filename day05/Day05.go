a66cc4b6af8ecd022cd56667401528d10c1a29ae
Petr Belyakov <keptelr@ya.ru>
2023-12-05 13:43:00 +0300

    day05 part2 completed

--- day05/Day05.go
+++ day05/Day05.go
@@ -8,6 +8,10 @@ import (
 )
 
 type Seed int
+type AnotherSeed struct {
+	from   int
+	length int
+}
 type SeedMap struct {
 	to     int
 	from   int
@@ -105,6 +109,16 @@ func parseSeeds(input string) (seeds []Seed) {
 	return
 }
 
+func parseAnotherSeeds(input string) (seeds []AnotherSeed) {
+	numDigits := utils.DigitsRegexp.FindAllString(input, -1)
+	for i := 1; i < len(numDigits); i += 2 {
+		seedFrom, _ := strconv.Atoi(numDigits[i-1])
+		lenght, _ := strconv.Atoi(numDigits[i])
+		seeds = append(seeds, AnotherSeed{from: seedFrom, length: lenght})
+	}
+	return
+}
+
 func parseMap(input string) (result SeedMap) {
 	numsStr := utils.DigitsRegexp.FindAllString(input, -1)
 	to, _ := strconv.Atoi(numsStr[0])
@@ -118,5 +132,66 @@ func parseMap(input string) (result SeedMap) {
 }
 
 func PartTwo(input []string) (result int) {
-	return 1
+	seeds := parseAnotherSeeds(input[0])
+	seedToSoil := make([]SeedMap, 0)
+	soilToFertilizer := make([]SeedMap, 0)
+	fertilizerToWater := make([]SeedMap, 0)
+	waterToLight := make([]SeedMap, 0)
+	lightToTemp := make([]SeedMap, 0)
+	temToHum := make([]SeedMap, 0)
+	humToLocation := make([]SeedMap, 0)
+
+	gap := 0
+	for _, value := range input[2:] {
+		if strings.Contains(value, "map") {
+			continue
+		}
+		if value == "" {
+			gap++
+			continue
+		}
+		if gap == 0 {
+			seedToSoil = append(seedToSoil, parseMap(value))
+		}
+		if gap == 1 {
+			soilToFertilizer = append(soilToFertilizer, parseMap(value))
+		}
+		if gap == 2 {
+			fertilizerToWater = append(fertilizerToWater, parseMap(value))
+		}
+		if gap == 3 {
+			waterToLight = append(waterToLight, parseMap(value))
+		}
+		if gap == 4 {
+			lightToTemp = append(lightToTemp, parseMap(value))
+		}
+		if gap == 5 {
+			temToHum = append(temToHum, parseMap(value))
+		}
+		if gap == 6 {
+			humToLocation = append(humToLocation, parseMap(value))
+		}
+	}
+
+	min := -1
+	for _, seed := range seeds {
+		for i := 0; i <= seed.length; i++ {
+			location := getFromMap(seed.from+i, seedToSoil)
+			location = getFromMap(location, soilToFertilizer)
+			location = getFromMap(location, fertilizerToWater)
+			location = getFromMap(location, waterToLight)
+			location = getFromMap(location, lightToTemp)
+			location = getFromMap(location, temToHum)
+			location = getFromMap(location, humToLocation)
+
+			if min < 0 {
+				min = location
+			}
+			if location < min {
+				min = location
+			}
+
+		}
+	}
+	return min
 }
--- day05/Day05_test.go
+++ day05/Day05_test.go
@@ -16,5 +16,5 @@ func TestPartOne(t *testing.T) {
 func TestPartTwo(t *testing.T) {
 	input := utils.ReadLines("test", "day05")
 	result := PartTwo(input)
-	assert.Equal(t, 1, result)
+	assert.Equal(t, 46, result)
 }
